edd1f459b90cc67e60c557bff47ac4e38c70de4e
LandrumA <landrumanderson02@gmail.com>
2025-05-06 13:34:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ this forces relative paths
+  base: '/', 
   plugins: [react()]
 })
+
