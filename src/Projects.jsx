78cc166e85ce1a574ac016aa5ca92c84dc640379
LandrumA <landrumanderson02@gmail.com>
2025-05-06 13:58:22 -0500

    asdadad

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,4 +1,10 @@
 import ProjectCard from './ProjectCard';
+import rhdImg from './images/rhd.png';
+import dysarthriaImg from './images/dysarthria.png';
+import tlineImg from './images/tline.png';
+import lpSolverImg from './images/lp_solver.png';
+import wassersteinImg from './images/wasserstein.png';
+
 
 export default function Projects() {
   const projects = [
@@ -7,35 +13,35 @@ export default function Projects() {
       description:
         "Developed a preprocessing pipeline using MFCC features to classify prosodic deficits from TalkBank audio. Cross-validated on speaker groups. Achieved 79% accuracy and 0.88 AUC using an RBF SVM.",
       tech: ["MFCC", "SVM", "TalkBank", "scikit-learn"],
-      image: "/images/rhd.jpg",
+      image: rhdImg,
     },
     {
       title: "Dysarthria Severity Classification (In Progress)",
       description:
         "Working on interpretable severity-level classification of dysarthric speech using wav2vec2, MFCC, Whisper. Goal: interpretability and fusion of acoustic embeddings.",
       tech: ["Deep learning", "Whisper", "Wav2Vec2", "MFCC"],
-      image: "/images/dysarthria.jpg",
+      image: dysarthriaImg,
     },
     {
       title: "Transmission Line Fault Location via Laplace Modeling",
       description:
         "Derived a model using Laplace transforms and Kirchhoff's Laws to estimate fault positions on transmission lines. Report includes original proofs and full derivations.",
       tech: ["Laplace", "Kirchhoff", "signal modeling", "applied math"],
-      image: "/images/tline.jpg",
+      image: tlineImg ,
     },
     {
       title: "Linear Programming Game Solver",
       description:
         "Implemented a matrix game solver using linear programming. Generalized the solution beyond class scope to handle arbitrary payoff matrices.",
       tech: ["Linear programming", "game theory", "NumPy"],
-      image: "/images/lp_solver.jpg",
+      image: lpSolverImg,
     },
     {
       title: "Wasserstein Barycenters on MNIST Digits",
       description:
         "Used optimal transport and the POT library to interpolate between MNIST digits and visualize barycenters in the Wasserstein space.",
       tech: ["Optimal transport", "POT", "MNIST", "Python"],
-      image: "/images/wasserstein.jpg",
+      image: lpSolverImg,
     },
   ];
 
